44cb62330bdd31cc15266216e4c3cb45a4312005
Dakuri Sri Harini Reddy <126244470+harinireddy0611@users.noreply.github.com>
2025-03-18 13:08:02 +0530

    Delete DailyTasks/2025-02-19 directory

--- DailyTasks/2025-02-19/Mongo-Spring/src/main/java/com/repo/Repo.java
+++
@@ -1,7 +0,0 @@
-package com.repo;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import com.model.Product;
-
-public interface Repo extends MongoRepository<Product, String> {
-}
